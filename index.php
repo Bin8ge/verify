aa2c07d8b2547c77cfe8c98faaefda01c29896f1
lbb <liangbinbin@chuanyuapp.com>
2022-06-28 21:01:55 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+use Bingo\Verify;
+
+header('content-type:text/html;charset=utf-8');
+
+include  './vendor/autoload.php';
+
+$var  = (new Verify)->isValidEmail('w2011344@qq.com');
+
+die(var_dump($var));  //测试一下
